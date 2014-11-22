2c379636f4f7105cae311d3df5a61651c4576761
ssilk <ssilk@users.noreply.github.com>
2014-11-22 10:47:44 -0800

    First passing spec
    spec/features_spec.js: User sees list of phones

--- spec/features_spec.js
+++ spec/features_spec.js
@@ -4,6 +4,7 @@ describe('Home page', function () {
   it('user sees list of phones', function () {
     browser.get('app/index.html');
     var phones = element.all(by.repeater('phone in phones'));
-    expect(phones.count() > 0).toBe(true);
+    phones.count().then(function (number) {console.log(number);});
+    expect(phones.count()).toBe(20);
   });
 });
